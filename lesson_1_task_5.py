9dc7532c933dec0852d53dcd8082d2f953d2e254
annacher28 <157986652+annacher28@users.noreply.github.com>
2025-06-03 20:41:15 +0300

    Update lesson_1_task_5.py

--- lesson_1_task_5.py
+++ lesson_1_task_5.py
@@ -1,17 +1,17 @@
 def print_num():
 	print(88005553535)
+	print(8)
+	print(0)
+	print(0)
+	print(5)
+	print(5)
+	print(5)
+	print(3)
+	print(5)
+	print(3)
+	print(5)
+print_num()
 
-print_num()
-print_num()
-print_num()
-print_num()
-print_num()
-print_num()
-print_num()
-print_num()
-print_num()
-print_num()
-print_num()
 
 
 
