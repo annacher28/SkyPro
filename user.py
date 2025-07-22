5046059d2f5f02cd7e3320b3860e40d2168482fc
Anna <ch3repanovaann@yandex.ru>
2025-07-22 22:25:27 +0300

    Задание 3

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+   def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+   def sayfirst_name(self):
+       print("Моё имя: ", self.first_name)
+   def seylast_name(self):
+       print("Моя фамилия: ", self.last_name)
+   def seylast_name(self):
+       print("Меня зовут - ", self.first_name, self.last_name)
+
+       
